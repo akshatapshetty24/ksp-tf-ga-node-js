bf5ae1b588c1d7b46f7c69c13a6158d48528a5a4
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-05-16 21:24:49 +0530

    Create variables.tf

---
+++ terraform/backend/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket for Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
+}
